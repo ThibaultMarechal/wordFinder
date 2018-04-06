d2392e97ad51546db38a9a209dd21dd75990116f
thibault <thibault.eric.marechal@gmail.com>
2018-04-06 02:14:34 +0200

    First commit

---
+++ wordFinder/src/wordFinder.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : wordFinder.cpp
+// Author      : Thibault MARECHAL
+// Version     :
+// Copyright   : Hey
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << ":D" << endl; // prints :D
+	cout << "llddl" << endl;
+	return 0;
+}
